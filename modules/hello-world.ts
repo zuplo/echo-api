4439997df47ddbe2af401bf78db1063493df4e8a
Josh Twist <josh@zuplo.com>
2022-02-28 17:14:57 -0800

    initial

---
+++ modules/hello-world.ts
@@ -0,0 +1,42 @@
+import { ZuploContext, ZuploRequest } from "@zuplo/runtime";
+
+const bodyValue = async (request: ZuploRequest, context: ZuploContext) => {
+
+  if (!request.body) {
+    return undefined;
+  }
+
+  const bodyText = await request.text();
+  const contentType = request.headers.get("content-type");
+
+
+  if (contentType.startsWith("application/json")) {
+    try {
+      return JSON.parse(bodyText);
+    }
+    catch (err) {
+      //ignore error
+    }
+  }
+  return bodyText;
+}
+
+export default async function (request: ZuploRequest, context: ZuploContext) {
+  const { url, method, query } = request;
+
+  const body = await bodyValue(request, context);
+
+  const headers = {};
+
+  for (const [key, value] of request.headers) {
+    headers[key] = value;
+  }
+
+  return {
+    url,
+    method,
+    query,
+    body,
+    headers
+  }
+}
